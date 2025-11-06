fc765502639c03e0412382b4654d3769eeb2ca9d
Alex Hanna <ahanna0608@gmail.com>
2025-11-06 16:07:08 -0500

    Initial Commit

---
+++ propeller/propellerApp.swift
@@ -0,0 +1,17 @@
+//
+//  propellerApp.swift
+//  propeller
+//
+//  Created by alex on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct propellerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
